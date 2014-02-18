a6c2b9638ae37dd23237a85f1db02cf945ea6538
Clark Lavery <calrk1@gmail.com>
2014-02-18 14:43:35 +1100

    allowing for two functions

--- index.js
+++ index.js
@@ -4,39 +4,45 @@ var map = require('map-stream')
 	, clone = require('clone')
 	, rename = require('gulp-rename');
 
-module.exports = function(options){
-	var opts = options ? options : {};
+module.exports = {
+	init: function(options){
 
-	function loadPartials(file, cb){
-		console.log(file.path);
-	}
+	},
 
-	function compile(file, cb){
-		var path = file.path;
-		var compiledTemplate = "asd";
-		if(path.match('partials') !== null){
-			basename = path.split('\\').pop().split('.').shift();
-			handlebars.registerPartial(basename, file.contents.toString());
-			return cb();
-		}
-		else{
-			compiledTemplate = handlebars.compile(file.contents.toString())();
+	compile: function(options){
+		var opts = options ? options : {};
+
+		function loadPartials(file, cb){
+			console.log(file.path);
 		}
 
-		var newFile = clone(file);
-		newFile.contents = new Buffer(compiledTemplate);
+		function compile(file, cb){
+			var path = file.path;
+			var compiledTemplate;
+			if(path.match('partials') !== null){
+				basename = path.split('\\').pop().split('.').shift();
+				handlebars.registerPartial(basename, file.contents.toString());
+				return cb();
+			}
+			else{
+				compiledTemplate = handlebars.compile(file.contents.toString())();
+			}
 
-		return cb(null, newFile);
+			var newFile = clone(file);
+			newFile.contents = new Buffer(compiledTemplate);
+
+			return cb(null, newFile);
+		}
+
+		var doRename = function(dir, base, ext) {
+			// Change the extension to .js
+			dir.extname = ".html";
+			return dir;
+		};
+
+		return es.pipeline(
+			map(compile),
+			rename(doRename)
+		);
 	}
-
-	var doRename = function(dir, base, ext) {
-		// Change the extension to .js
-		dir.extname = ".html";
-		return dir;
-	};
-
-	return es.pipeline(
-		map(compile),
-		rename(doRename)
-	);
 };
