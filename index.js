54b13a4fe3070089f8ba9fa08f5798bcd1cda05b
Clark Lavery <calrk1@gmail.com>
2014-02-18 18:08:54 +1100

    current working version, has special needs though.

--- index.js
+++ index.js
@@ -4,50 +4,35 @@ var map = require('map-stream')
 	, clone = require('clone')
 	, rename = require('gulp-rename');
 
-module.exports = {
-	precompile: function(options){
-		function compile(file, cb){
-			var path = file.path;
-			if(path.match('partials') !== null){
-				basename = path.split('\\').pop().split('.').shift();
-				handlebars.registerPartial(basename, file.contents.toString());
-			}
-			return cb(null, file);
+module.exports = function(options){
+	var opts = options ? options : {};
+
+	function compile(file, cb){
+		var path = file.path;
+		var compiledTemplate;
+		if(path.match('partials') !== null){
+			basename = path.split('\\').pop().split('.').shift();
+			handlebars.registerPartial(basename, file.contents.toString());
+			return cb();
 		}
-		return es.pipeline(
-			map(compile)
-		);
-	},
-
-	compile: function(options){
-		var opts = options ? options : {};
-
-		function compile(file, cb){
-			var path = file.path;
-			var compiledTemplate;
-			if(path.match('partials') !== null){
-				return cb();
-			}
-			else{
-				compiledTemplate = handlebars.compile(file.contents.toString())();
-				
-			}
-
-			var newFile = clone(file);
-			newFile.contents = new Buffer(compiledTemplate);
-
-			return cb(null, newFile);
+		else{
+			compiledTemplate = handlebars.compile(file.contents.toString())();
 		}
 
-		var doRename = function(dir, base, ext) {
-			// Change the extension to .js
-			dir.extname = ".html";
-			return dir;
-		};
+		var newFile = clone(file);
+		newFile.contents = new Buffer(compiledTemplate);
 
-		return es.pipeline(
-			map(compile),
-			rename(doRename)
-		);
+		return cb(null, newFile);
 	}
+
+	var doRename = function(dir, base, ext){
+		// Change the extension to .js
+		dir.extname = ".html";
+		return dir;
+	};
+
+	return es.pipeline(
+		map(compile),
+		rename(doRename)
+	);
 };
